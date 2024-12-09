bf3dd34fcd28829699db21da341473ce77d4b97d
Bryan Arredondo <bryanpool991@hotmail.com>
2024-12-09 12:55:26 -0500

    imit project wasifitpe

---
+++ wasi-fit-pe-app/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBoR39-OU4cdG1E3AIb0fWY6lF1l_Dh9Lk",
+    authDomain: "wasi-fit-pe-app.firebaseapp.com",
+    databaseURL: "https://wasi-fit-pe-app-default-rtdb.firebaseio.com",
+    projectId: "wasi-fit-pe-app",
+    storageBucket: "wasi-fit-pe-app.firebasestorage.app",
+    messagingSenderId: "110399370438",
+    appId: "1:110399370438:web:3e8f7f4f32e946b83b93c7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
